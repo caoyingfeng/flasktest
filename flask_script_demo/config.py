790a985e89630b1ee4a79532341b09e8db9bc591
caoyingfeng <164972038@qq.com>
2019-05-02 14:17:51 +0800

    flask_script_demo

---
+++ flask_script_demo/config.py
@@ -0,0 +1,11 @@
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'flask_script_demo'
+USERNAME = 'root'
+PASSWORD = '199232cyl'
+
+# dialect+driver://username:password@host:port/database
+DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8"\
+    .format(username=USERNAME,password=PASSWORD,host=HOSTNAME,port=PORT,db=DATABASE)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
